870ec1a369044eb3bc8bfcdb78062f2d0e8f90d6
Gastón García <gaston.garcia@gmail.com>
2018-12-28 20:40:56 -0600

    Removing node_modules from commits. Added handlebars. Added bootstrap files

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//set the path to the public folder for static assets
 app.use(express.static(path.join(__dirname, 'public')));
+
+//set the path to the views folder
 app.set('views', path.join(__dirname, 'views'));
+
+//use express-handlebars as the views engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 
 app.get('/', (req,res) => {
-    res.send("Hello world");
+    const pageTitle = "Home";
+    res.render("index", {
+        pageTitle: pageTitle
+    })
 });
 
 
