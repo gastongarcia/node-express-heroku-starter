d9a41f0ba91cf46100c5dc637ba7d1c062f96252
Gastón García <gaston.garcia@gmail.com>
2018-12-28 18:51:17 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req,res) => {
+    res.send("Hello world");
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
